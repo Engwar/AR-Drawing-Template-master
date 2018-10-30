59402a3c1903937a87c144b89dab3b5ec198494d
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-10-30 15:38:55 +0300

    Initial Commit

---
+++ ARKit-Drawing/ViewController.swift
@@ -0,0 +1,142 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    var selectedNode: SCNNode?
+    
+    var placedNodes = [SCNNode]()
+    var planeNodes = [SCNNode]()
+    
+    enum ObjectPlacementMode {
+        case freeform, plane, image
+    }
+    
+    var objectMode: ObjectPlacementMode = .freeform {
+        didSet {
+            reloadCOnfiguration()
+        }
+    }
+    
+ 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reloadCOnfiguration()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+    func reloadCOnfiguration() {
+        configuration.detectionImages = (objectMode == .image) ? ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) : nil
+        sceneView.session.run(configuration)
+    }
+    
+    @IBAction func changeObjectMode(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            objectMode = .freeform
+        case 1:
+            objectMode = .plane
+        case 2:
+            objectMode = .image
+        default:
+            break
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showOptions" {
+            let optionsViewController = segue.destination as! OptionsContainerViewController
+            optionsViewController.delegate = self
+        }
+    }
+}
+
+//MARK: OptionViewControllerDelegate
+extension ViewController: OptionsViewControllerDelegate {
+    
+    func objectSelected(node: SCNNode) {
+        dismiss(animated: true, completion: nil)
+        selectedNode = node
+    }
+    
+    func togglePlaneVisualization() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func undoLastObject() {
+        
+    }
+    
+    func resetScene() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+//MARK: Touches Managment
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        guard let node = selectedNode, let _ = touches.first else { return }
+        
+        switch objectMode{
+        case .freeform:
+            addNodeInFront(node)
+        case .plane:
+            break
+        case .image:
+            break
+        }
+    }
+}
+
+//MARK: Placement Methods
+extension ViewController {
+    func addNodeInFront(_ node: SCNNode) {
+        guard let frame = sceneView.session.currentFrame else {return}
+        
+        let transform = frame.camera.transform
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.2
+        
+        node.simdTransform = matrix_multiply(transform, translation)
+        
+        addNodeToSceneRoot(node)
+    }
+    
+    func addNodeToSceneRoot(_ node: SCNNode) {
+        let cloneNode = node.clone()
+        sceneView.scene.rootNode.addChildNode(cloneNode)
+        placedNodes.append(cloneNode)
+    }
+
+}
+
+//MARK: ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let anchor = anchor as? ARImageAnchor {
+            
+        } else if let anchor = anchor as? ARPlaneAnchor {
+            
+        }
+    }
+}
